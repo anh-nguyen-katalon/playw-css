921ec5fd4148407c0babacada322318acaaea43d
anh-nguyen-katalon <anh.hnnguyen@katalon.com>
2024-01-30 15:00:30 +0700

    update

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ import json
 
 def preprocess_href(href, home_page):
     # turn relative urls into absolute urls
+    home_page = re.sub(r'/+$', '', home_page) # remove trailing slash
     if href.startswith("/"):
-        href = home_page[:-1] + href
+        href = home_page + href
     # remove query parameters
     query_index = href.find("?")
     href = href[:query_index] if query_index != -1 else href
@@ -36,29 +37,36 @@ with sync_playwright() as p:
     q.put(home_page)
 
     discovered_urls.add(home_page)
-    while not q.empty() and num_visited < 5:
-        url = q.get()
-        page.goto(url, wait_until="networkidle")
-        print (url)
-        num_visited += 1
+    while not q.empty() and num_visited < 3:
+        try:
+            url = q.get()
+            print (url)
 
-        # get role tree
-        page.add_script_tag(content=playw_css_script)
-        role_tree = page.evaluate(role_tree_script + "getRoleTree();")
-        
-        # write role tree to file
-        dir_path = url.replace(home_page, "")
-        dir_path = re.sub(r'^/+|/+$', '', dir_path)
-        os.makedirs(os.path.join("output", dir_path), exist_ok=True)
-        with open(f"output/{dir_path}/role_tree.json", "w") as f:
-            f.write(json.dumps(role_tree, indent=2))
+            print ("Going to page")
+            page.goto(url, wait_until="networkidle")
 
-        # discover child urls
-        for a in page.locator("a").all():
-            href = a.get_attribute("href")
-            if href is not None:
-                href = preprocess_href(href, home_page)
-                if href.startswith(home_page) and href not in discovered_urls:
-                    discovered_urls.add(href)
-                    q.put(href)
+            num_visited += 1
+
+            # get role tree
+            page.add_script_tag(content=playw_css_script)
+            print ("Getting role tree")
+            role_tree = page.evaluate(role_tree_script + "getRoleTree();")
+            
+            # write role tree to file
+            dir_path = re.sub(r'^https?://', '', url) # remove protocol
+            dir_path = re.sub(r'/+$', '', dir_path) # remove trailing slash
+            os.makedirs(os.path.join("output", dir_path), exist_ok=True)
+            with open(f"output/{dir_path}/role_tree.json", "w") as f:
+                f.write(json.dumps(role_tree, indent=2))
+
+            # discover child urls
+            for a in page.locator("a").all():
+                href = a.get_attribute("href")
+                if href is not None:
+                    href = preprocess_href(href, home_page)
+                    if href.startswith(home_page) and href not in discovered_urls:
+                        discovered_urls.add(href)
+                        q.put(href)
+        except Exception as e:
+            print (url, e)
     browser.close()
